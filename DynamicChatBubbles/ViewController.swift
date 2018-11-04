8bde5cfe5799652b2870784a36367e4c9f3b277d
Mehmet Salih Koçak <kocaksalihm@gmail.com>
2018-11-04 09:36:25 +0300

    Initial Commit

---
+++ DynamicChatBubbles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicChatBubbles
+//
+//  Created by Mehmet Salih Koçak on 4.11.2018.
+//  Copyright © 2018 Mehmet Salih Koçak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
